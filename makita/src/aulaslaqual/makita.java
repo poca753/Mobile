3a8fcc8440d6e1bffdb954c415440574be199c3a
poca753 <165678402+poca753@users.noreply.github.com>
2025-06-30 12:25:10 -0300

    jh

---
+++ makita/src/aulaslaqual/makita.java
@@ -0,0 +1,12 @@
+package aulaslaqual;
+
+public class makita{
+    public static void main(String[] args) {
+            String nome;
+            String descrição;
+            String autor;
+            String isbn;
+            double preço;
+            String datapub;
+        }
+    }
