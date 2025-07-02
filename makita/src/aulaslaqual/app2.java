b71dc8a1b7593ad603c586161c2fd35434413a24
poca753 <165678402+poca753@users.noreply.github.com>
2025-07-02 08:08:46 -0300

    gfdg

---
+++ makita/src/aulaslaqual/app2.java
@@ -0,0 +1,13 @@
+package aulaslaqual;
+
+public class app2 {
+    public static void main(String[] args) {
+        app livro = new app();
+
+        livro.nome = "O senhor dos aneis";
+        livro.descrição = "Uma obra literaria de fantasia";
+        livro.isbn = "435325645645";
+        livro.preço = 39.90;
+        livro.autor = "J. R. R. Tolkien";
+    }
+}
