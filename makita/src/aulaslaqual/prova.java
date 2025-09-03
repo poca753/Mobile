873fa65cce82e4543ebf11f894b779735c8b262f
poca753 <165678402+poca753@users.noreply.github.com>
2025-09-03 11:31:25 -0300

    vfd
    fgg

---
+++ makita/src/aulaslaqual/prova.java
@@ -0,0 +1,13 @@
+package aulaslaqual;
+
+public class prova {
+    public static void main(String[] args) {
+         int variavel = 1;
+
+        while (variavel <=10) {
+            System.out.println(variavel);
+            variavel ++ ;
+        }
+        System.out.println("variavel chegou a 10!");
+    }
+}
