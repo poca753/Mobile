ee10dbb9c6b001ef92acc4b2c954bfea4c0d0577
poca753 <165678402+poca753@users.noreply.github.com>
2025-09-03 12:27:46 -0300

    ythynygt
    hythyth

---
+++ makita/src/aula/App2.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class App2 {
+
+public static void main(String[] args) {
+app livro = new app();
+
+livro.nome = "O senhor dos aneis";
+livro.descricao = "Uma obra literaria de fantasia";
+livro.isbn = "9783161484100";
+livro.preco = 39.90;
+livro.autor = "j.r.r. tolkien";
+livro.datapub = "1954";
+
+dadosLivros()
+}
+}
---
+++ makita/src/aula/app.java
@@ -0,0 +1,15 @@
+package aula;
+
+public class app{
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+void dadosLivros(){
+    System.out.println("nome do livro" + nome);
+    System.out.println("preço" + preço);
+}
+}
