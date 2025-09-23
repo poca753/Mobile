c5549ffeb05bd4062ba8bc1bd37a1e3a5f4888bb
poca753 <165678402+poca753@users.noreply.github.com>
2025-09-23 09:54:58 -0300

    fgrfg
    gfrtgvff

---
+++ makita/src/prova/prova.java
@@ -0,0 +1,13 @@
+package prova\aula;
+
+public class prova {
+    public static void main(String[] args) {
+        
+      String autor;
+      String descrição;
+
+ void dadosLivros()
+      System.out.println("autor " + autor);
+      System.out.println("descrição " + descrição);
+    }
+}
---
+++ makita/src/prova/prova2.java
@@ -0,0 +1,15 @@
+package prova;
+
+import aula.app;
+
+public class prova2 {
+    
+    public static void main(String[] args) {
+        app livro = new app();
+
+        Livro.autor = J.K.Rowling;
+        livro.descricao = saga de livros de magia e fantasia
+
+        livro.dadosLivros();
+    }
+}
